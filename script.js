55c128ad675bc88fa247731ef38b7ab5f70daa34
ElenaGireva <elena.webcode@gmail.com>
2024-02-04 16:28:04 +0100

    added comments to debagg

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const calculateBonus = (a, b) => {
-    let bonus;
-    const sum = a + b;
-    debugger;
-    sum > 50 ? (bonus = 50) : (bonus = sum);
-    debugger;
-    return bonus;
-    console.log(bonus)
-    };
+  let bonus;
+  const sum = a + b;
+  //сравниваем сумму двух заданных чисел с 50, если сумма больше 50, то переменная Бонус = 50, иначе переменная Бонус = сумме этих чисел
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  //возвращаем вычесленную переменную Бонус
+  return bonus;
+};
 
-calculateBonus(2,5)
-
+calculateBonus(2, 5);

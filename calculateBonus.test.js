2ad300b9f895fe2cc2170e2f0d8a29515f48aa07
ElenaGireva <elena.webcode@gmail.com>
2024-02-07 12:42:37 +0100

    added different values

---
+++ calculateBonus.test.js
@@ -0,0 +1,19 @@
+const calculateBonus = require("./script");
+
+it("Amount less than 50 ", () => {
+  expect(calculateBonus(-20, 50)).toBe(30);
+  expect(calculateBonus(0, 0)).toBe(0);
+  expect(calculateBonus(40, 9)).toBe(50);
+});
+
+it("Amount more than 50 ", () => {
+  expect(calculateBonus(30, 25)).toBe(50);
+  expect(calculateBonus(40, 11)).toBe(50);
+  expect(calculateBonus(-30, 90)).toBe(50);
+});
+it("Amount equals 50 ", () => {
+  expect(calculateBonus(30, 20)).toBe(50);
+  expect(calculateBonus(50, 0)).toBe(50);
+  expect(calculateBonus(-50, 100)).toBe(50);
+});
+
--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// function calculateBonus(a,b) {
-//   let bonus;
-//   const sum = a + b;
-//   sum > 50 ? (bonus = 50) : (bonus = sum);
-//   return bonus;
-// };
+function calculateBonus(a,b) {
+  let bonus;
+  const sum = a + b;
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  return bonus;
+};
 
-function sum(a,b){
-  return a+b;
-}
 
-module.exports = sum;
+module.exports = calculateBonus;
